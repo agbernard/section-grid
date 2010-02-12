9ef44c80126564751e54ea1b70724cfd07421719
Andrew Bernard <agbernard@gmail.com>
2010-02-11 20:23:21 -0500

    Updates to the implementation

--- src/com/agbdev/sectiongrid/SwingSectionGrid.java
+++ src/com/agbdev/sectiongrid/SwingSectionGrid.java
@@ -1,20 +1,25 @@
 package com.agbdev.sectiongrid;
 
 import java.awt.BorderLayout;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SpringLayout;
 import javax.swing.table.AbstractTableModel;
+import com.agbdev.sectiongrid.GridSection.ID;
 
 public class SwingSectionGrid
 implements SectionGrid {
 	private JPanel pnlMain;
-	private int numSections = 0;
+	private Map<ID, GridSection> sections;
 
 	public SwingSectionGrid() {
     	this.pnlMain = new JPanel(new SpringLayout());
+    	this.sections = new HashMap<ID, GridSection>();
 	}
 
 	public void addSection(final GridSection section) {
@@ -22,51 +27,57 @@ implements SectionGrid {
     	JTable table = new JTable(new GridModel(model));
     	JScrollPane scrollPane = new JScrollPane(table);
     	table.setFillsViewportHeight(true);
-    	
+
     	JPanel panel = new JPanel(new BorderLayout());
     	panel.add(new JLabel(section.getName()), BorderLayout.PAGE_START);
     	panel.add(scrollPane, BorderLayout.CENTER);
     	this.pnlMain.add(panel);
-    	this.numSections++;
+    	this.sections.put(section.getId(), section);
 	}
-	
+
 	private final class GridModel extends AbstractTableModel {
 		private static final long serialVersionUID = 80394553666744922L;
 		private GridDataModel model;
-		
-		private GridModel(GridDataModel model) {
+
+		private GridModel(final GridDataModel model) {
 			this.model = model;
 		}
 
-		public String getColumnName(int col) {
-	        return model.getColumnName(col);
+		@Override
+        public String getColumnName(final int col) {
+	        return this.model.getColumnName(col);
 	    }
 
-		
+
 		@Override
 		public int getColumnCount() {
-			return model.getNumColumns();
+			return this.model.getNumColumns();
 		}
 
 		@Override
 		public int getRowCount() {
-			return model.getNumRows();
+			return this.model.getNumRows();
 		}
 
 		@Override
-		public Object getValueAt(int rowIndex, int colIndex) {
-			return model.getValueAt(rowIndex, colIndex);
+		public Object getValueAt(final int rowIndex, final int colIndex) {
+			return this.model.getValueAt(rowIndex, colIndex);
 		}
-		
-	}	
+
+	}
 
 	@Override
     public Object getImplementation() {
 		SpringUtilities.makeCompactGrid(this.pnlMain,
-		                                this.numSections, 1, //rows, cols
+		                                this.sections.size(), 1, //rows, cols
 		                                6, 6,        //initX, initY
 		                                6, 6);       //xPad, yPad
 	    return this.pnlMain;
+    }
+
+	@Override
+    public void updateSection(final ID id, final List<Row> data) {
+	    this.sections.get(id).update(data);
     }
 
 }
--- test/com/agbdev/sectiongrid/TestSection.java
+++ test/com/agbdev/sectiongrid/TestSection.java
@@ -1,41 +1,44 @@
 package com.agbdev.sectiongrid;
 
+import java.util.List;
+
 
 public class TestSection implements GridSection {
 	private int rowCount = 0;
-	
+
+	private GridDataModel model = new GridDataModel(Col.values());
+
 	@Override
     public String getName() {
 	    return "Section 1";
     }
-	
+
 	@Override
     public GridDataModel getDataModel() {
-		GridDataModel model = new GridDataModel(Col.values());
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-		model.add(getFakeRow(rowCount++));
-	    return model;
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+		this.model.add(getFakeRow(this.rowCount++));
+	    return this.model;
     }
-	
+
 	private Row getFakeRow(final int rowCount) {
 		return new Row() {
-			
+
 			@Override
-			public Object getValue(Column col) {
+			public Object getValue(final Column col) {
 				return "Row "+ rowCount + " data";
 			}
 		};
 	}
-	
+
 	private enum Col implements Column {
 		COL1 {
 			@Override
@@ -43,7 +46,7 @@ public class TestSection implements GridSection {
 				return "Column 1";
 			}
 		},
-		
+
 		COL2 {
 			@Override
 			public String getTitleString() {
@@ -51,7 +54,21 @@ public class TestSection implements GridSection {
 			}
 		};
 
-		
+		@Override
+	    public int index() {
+	        return ordinal();
+	    }
+
 	}
+
+	@Override
+    public ID getId() {
+	    return new ID() {};
+    }
+
+	@Override
+    public void update(final List<Row> data) {
+	    this.model.update(data);
+    }
 
 }
